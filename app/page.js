2aeeed7bb9cf4fcfe62b3f15af5486f85584d3e6
NikBK <nikhilbalakrishna5@gmail.com>
2023-06-05 20:00:52 +0530

    removes extra code

--- app/page.js
+++ app/page.js
@@ -2,22 +2,6 @@ import Form from '@components/Form';
 import Tracker from '@components/Tracker';
 
 export default function Home() {
-  const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-      }
-    ]
-  };
 
   return (
     <main className="w-full h-screen items-center flex justify-around flex-wrap">
@@ -29,7 +13,6 @@ export default function Home() {
 
       <Tracker
         title="Expense"
-        data={data}
       />
     </main>
   )
